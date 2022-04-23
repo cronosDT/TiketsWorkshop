cbf40373f7f1249aa47d8d4bedd925c0214b9c5b
HARRISON ALONSO ARROYAVE GAVIRIA <harrisonarroyave286597@correo.itm.edu.co>
2022-04-23 05:03:38 -0500

    Changes2-Style

--- TicketsWorkshop/TicketsWorkshop/Program.cs
+++ TicketsWorkshop/TicketsWorkshop/Program.cs
@@ -42,6 +42,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Ticket}/{action=Index}/{id?}");
 
 app.Run();
